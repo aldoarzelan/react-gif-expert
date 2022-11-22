c57f947491299a5b84804b1929703695886fe971
aldo.arzelan@jbknowledge.com <aldo.arzelan@jbknowledge.com>
2022-11-22 15:46:19 -0300

    last commit

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types';
 
 
 export const AddCategory = ({ onNewCategory }) => {
@@ -25,7 +26,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input 
             type="text"
             placeholder="Buscar gifs"
@@ -35,4 +36,8 @@ export const AddCategory = ({ onNewCategory }) => {
         </form>
         
     )
+}
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
